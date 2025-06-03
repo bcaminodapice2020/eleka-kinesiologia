8b142382bdb4e07ea2608a6cd928b2b40fe5490c
bcaminodapice2020 <bautistagit01@gmail.com>
2025-06-03 15:47:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,17 @@ document.getElementById("form-turno").addEventListener("submit", function(event)
     document.getElementById("form-turno").reset();
 });
 
+window.onscroll = function () {
+  const btn = document.getElementById("btn-volver-arriba");
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+
+document.getElementById("btn-volver-arriba").addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
